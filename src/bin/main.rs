6bdb776e8a3c8d7734b82c429971187b1e77110b
sylvan <sylvan@plentify.co.za>
2019-10-28 22:51:10 +0200

    Added check for image

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,14 +4,11 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 
 use std::thread;
-use std::time::Duration;
 
 use hello::ThreadPool;
 
 fn main() {
-    let ip_address = fs::read_to_string("ip_address.txt").unwrap();
-
-    let listener = TcpListener::bind(ip_address).unwrap();
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(4);
 
     for stream in listener.incoming().take(2) {
@@ -28,6 +25,7 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let image = read_in_image();
 
+    println!("Image read in");
     loop {
         stream.write(&image[0..64]).unwrap();
 
